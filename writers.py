17e12f41e8ffcf8c55b06af8b7276933d64e8460
Alexey Chernyshev <topepo45@gmail.com>
2022-07-03 19:02:59 +0500

    подготовка данных для записи в файл.

--- writers.py
+++ writers.py
@@ -1,3 +1,12 @@
+import csv
+
+
+def write_file(data: list[list]) -> None:
+    with open('out.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        writer.writerows(data)
+
+
 def write_data_csv(
         regions: list[dict],
         years: tuple[str],
@@ -19,24 +28,6 @@ def write_data_csv(
         data.append(
             [name, 'балл', *years_result]
         )
-
-
     print(data)
 
-
-
-
-
-if __name__ == '__main__':
-    print(get_keys_years(
-        {
-            "dim0": "Средний индекс качества городской среды по Российской Федерации (субъекту Российской Федерации) за отчетный год, в баллах (округляется до целого значения)",
-            "dim3301": "Российская Федерация",
-            "dim30611": "балл",
-            "dim2018_1558883_d77585884_i2184": "163",
-            "dim2019_1558883_d77585884_i2185": "169",
-            "dim2020_1558883_d77585884_i2195": "177"
-        },
-        ('2018', '2019', '2020')
-    ))
-
+    write_file(data)
